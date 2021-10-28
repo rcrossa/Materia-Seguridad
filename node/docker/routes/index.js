dc2201d064f955c6e5d085aa4fabb06f1e7dbca8
rcrossa <rcrossa@hotmail.com>
2021-10-27 22:34:14 -0300

    Update

--- node/docker/routes/index.js
+++ node/docker/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express with docker test'});
+  res.render('index', { title: 'Express with docker test 123'});
 });
 
 module.exports = router;
